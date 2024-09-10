854ac159273d7cbb53c35db1dc1e68508b6c3564
TeoAlex <teoalexsin@gmail.com>
2024-09-10 13:26:41 +0300

    vbump & changelog

--- strong-testimonials-migrate-away-from-easy-testimonials.php
+++ strong-testimonials-migrate-away-from-easy-testimonials.php
@@ -5,7 +5,7 @@
  * Description: Submodule that helps migrate testimonials from Easy Testimonials to Strong Testimonials
  * Author: WPChill
  * Author URI: https://www.wpchill.com/
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
 // Exit if accessed directly.
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPMTST_ET_MIGRATOR_VERSION', '1.0.0' );
+define( 'WPMTST_ET_MIGRATOR_VERSION', '1.0.1' );
 define( 'WPMTST_ET_MIGRATOR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WPMTST_ET_MIGRATOR_URL', plugin_dir_url( __FILE__ ) );
 define( 'WPMTST_ET_MIGRATOR_FILE', __FILE__ );
